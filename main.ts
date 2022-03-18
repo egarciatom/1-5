6b962bc0d4eca7944aafa0c25a6d997cddde40bd
egarciatom <101406426+egarciatom@users.noreply.github.com>
2022-03-18 14:05:31 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltLeft)) {
+        basic.showArrow(ArrowNames.North)
+        basic.showArrow(ArrowNames.NorthEast)
+        basic.showArrow(ArrowNames.East)
+        basic.showArrow(ArrowNames.SouthEast)
+        basic.showArrow(ArrowNames.West)
+        basic.showArrow(ArrowNames.NorthWest)
+    }
+    if (input.isGesture(Gesture.TiltRight)) {
+        basic.showArrow(ArrowNames.North)
+        basic.showArrow(ArrowNames.NorthEast)
+        basic.showArrow(ArrowNames.East)
+        basic.showArrow(ArrowNames.SouthEast)
+        basic.showArrow(ArrowNames.West)
+        basic.showArrow(ArrowNames.NorthWest)
+    }
+})
